e06f359dd508b4a14b1a390eb200ace54033cca4
jmangam <jmangam010@g.educaand.es>
2022-01-25 13:01:13 +0100

    JMG-Generando Items y personaje del juego parte 2

--- src/main/java/es/javimg/proyectojuegojavi/App.java
+++ src/main/java/es/javimg/proyectojuegojavi/App.java
@@ -5,6 +5,7 @@ import javafx.animation.Timeline;
 import javafx.application.Application;
 import static javafx.application.Application.launch;
 import javafx.event.ActionEvent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,13 +22,17 @@ public class App extends Application {
     
     int imgX = 0;
     int img1X = 998;
-    
-    //imagen de fondo
+    int contazulX = 0;
+    //imagen de fondo 1
     Image img = new Image(getClass().getResourceAsStream("/images/imagentop.jpg"));
     ImageView imgView = new ImageView(img);
-        
+    //imagen de fondo 2
     Image img1 = new Image(getClass().getResourceAsStream("/images/imagentop1.jpg"));
     ImageView img1View = new ImageView(img);
+    //gif del personaje
+    Image img2 = new Image(getClass().getResourceAsStream("/images/personaje.gif"));
+    ImageView img2View = new ImageView(img2);
+    
     
 
     @Override
@@ -38,40 +43,104 @@ public class App extends Application {
         stage.setTitle("JuegoJavi");
         stage.show();
         
+        //añadimos las 3 imagenes en pantalla
         paneRoot.getChildren().add(imgView);
         paneRoot.getChildren().add(img1View);
+        paneRoot.getChildren().add(img2View);
+        img2View.setX(10);
+        img2View.setY(265);
+        img2View.setScaleX(0.25);
+        img2View.setScaleY(0.25);
+        
+        //dibujo contenedor verde
+        Rectangle parteabajo = new Rectangle ();
+        parteabajo.setWidth(50);
+        parteabajo.setHeight(33);
+        parteabajo.setX(5);
+        parteabajo.setY(30);
+        parteabajo.setFill(Color.GREEN);
+        
+        Circle partearriba = new Circle();
+        partearriba.setRadius(25); 
+        partearriba.setCenterX(30);
+        partearriba.setCenterY(30);
+        partearriba.setFill(Color.GREEN);
+        
+        Circle agujero = new Circle();
+        agujero.setRadius(7); 
+        agujero.setCenterX(30);
+        agujero.setCenterY(25);
+        agujero.setFill(Color.BLACK);
+        
+        Group contverde = new Group();
+        contverde.getChildren().add(parteabajo);
+        contverde.getChildren().add(partearriba);
+        contverde.getChildren().add(agujero);
+        
+        //paneRoot.getChildren().add(contverde);
+        
+        //dibujo contenedor azul
+        Rectangle parteabajo1 = new Rectangle ();
+        parteabajo1.setWidth(50);
+        parteabajo1.setHeight(33);
+        parteabajo1.setX(5);
+        parteabajo1.setY(30);
+        parteabajo1.setFill(Color.BLUE);
+        
+        Circle partearriba1 = new Circle();
+        partearriba1.setRadius(25); 
+        partearriba1.setCenterX(30);
+        partearriba1.setCenterY(30);
+        partearriba1.setFill(Color.BLUE);
+        
+        Circle agujero1 = new Circle();
+        agujero1.setRadius(7); 
+        agujero1.setCenterX(30);
+        agujero1.setCenterY(25);
+        agujero1.setFill(Color.BLACK);
 
-        Rectangle cuerpo = new Rectangle ();
-        cuerpo.setWidth(50);
-        cuerpo.setHeight(33);
-        cuerpo.setX(5);
-        cuerpo.setY(30);
-        cuerpo.setFill(Color.GREEN);
+        Group contazul = new Group();
+        contazul.getChildren().add(parteabajo1);
+        contazul.getChildren().add(partearriba1);
+        contazul.getChildren().add(agujero1);
         
-        Circle cabeza = new Circle();
-        cabeza.setRadius(25); 
-        cabeza.setCenterX(30);
-        cabeza.setCenterY(30);
-        cabeza.setFill(Color.GREEN);
+        //paneRoot.getChildren().add(contazul);
+
+        //dibujo contenedor amarillo
+        Rectangle parteabajo2 = new Rectangle ();
+        parteabajo2.setWidth(50);
+        parteabajo2.setHeight(33);
+        parteabajo2.setX(5);
+        parteabajo2.setY(30);
+        parteabajo2.setFill(Color.YELLOW);
         
-        Circle ojo = new Circle();
-        ojo.setRadius(7); 
-        ojo.setCenterX(30);
-        ojo.setCenterY(30);
-        ojo.setFill(Color.BLACK);
+        Circle partearriba2 = new Circle();
+        partearriba2.setRadius(25); 
+        partearriba2.setCenterX(30);
+        partearriba2.setCenterY(30);
+        partearriba2.setFill(Color.YELLOW);
         
+        Circle agujero2 = new Circle();
+        agujero2.setRadius(7); 
+        agujero2.setCenterX(30);
+        agujero2.setCenterY(25);
+        agujero2.setFill(Color.BLACK);
         
-        paneRoot.getChildren().add(cuerpo);
-        paneRoot.getChildren().add(cabeza);
-        paneRoot.getChildren().add(ojo);
+        Group contamarillo = new Group();
+        contamarillo.getChildren().add(parteabajo2);
+        contamarillo.getChildren().add(partearriba2);
+        contamarillo.getChildren().add(agujero2);
+
+        //paneRoot.getChildren().add(contamarillo);
+        
+
         
         Timeline animationpaisaje = new Timeline(
             new KeyFrame(Duration.seconds(0.017), (ActionEvent ae) -> {
-                //System.out.println(desiertoX);
+
                 imgView.setX(imgX);
                 imgX -= 5;
                
-                //System.out.println(desiertoX2);
                 img1View.setX(img1X);
                 img1X -= 5;
                
@@ -82,7 +151,7 @@ public class App extends Application {
                 if (img1X < -990) {
                 img1X = 998;
                 }
-               
+                
             })
         );
         animationpaisaje.setCycleCount(Timeline.INDEFINITE);
